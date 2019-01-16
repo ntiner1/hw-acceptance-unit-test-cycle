580b1514eb72e286fcbc17fb6c40144e2cde579a
Nick <ntiner1@tamu.edu>
2019-01-16 22:01:58 +0000

    First cucumber test passing

--- rottenpotatoes/features/step_definitions/movies_steps.rb
+++ rottenpotatoes/features/step_definitions/movies_steps.rb
@@ -23,3 +23,7 @@ Then /I should see all the movies/ do
     step %{I should see "#{movie.title}"}
   end
 end
+
+Then /the director of "(.*)" should be "(.*)"/ do |movie, name|
+  expect(Movie.find_by_title(movie).director).to eq name
+end
